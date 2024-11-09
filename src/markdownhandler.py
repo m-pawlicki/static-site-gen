597a583836765debc6a974386d4b9a31748d3587
Micah Pawlicki <mich.pawlicki@gmail.com>
2024-11-08 23:20:17 -0600

    working on splitting images and links

--- src/markdownhandler.py
+++ src/markdownhandler.py
@@ -1,33 +1,51 @@
-from textnode import TextNode, TextType
+from textnode import *
 import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        nodes = []
-        for node in old_nodes:
-            if node.text_type != "text":
-                nodes.append(node)
+    nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            nodes.append(node)
+            continue
+        split = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise Exception("Invalid markdown syntax.")
+        for i in range(len(sections)):
+            if sections[i] == "":
                 continue
-            split = []
-            sections = node.text.split(delimiter)
-            if len(sections) % 2 == 0:
-                 raise Exception("Invalid markdown syntax.")
-            for i in range(len(sections)):
-                if sections[i] == "":
-                      continue
-                if i % 2 == 0:
-                      split.append(TextNode(sections[i], TextType.TEXT))
-                else:
-                     split.append(TextNode(sections[i], text_type))
-            nodes.extend(split)
+            if i % 2 == 0:
+                split.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split.append(TextNode(sections[i], text_type))
+        nodes.extend(split)
             
-        return nodes
+    return nodes
 
 def extract_markdown_images(text):
      return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 def split_nodes_image(old_nodes):
-     pass
+    nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            nodes.append(node)
+            continue
+        split = []
+        extract = extract_markdown_images(node.text)
+        if len(extract) == 0:
+            return old_nodes
+        for pair in extract:
+            alt_txt = pair[0]
+            img_url = pair[1]
+            sections = node.text.split(f"![{alt_txt}]({img_url})", 1)
+            print(sections)
+        nodes.extend(split)
+    
+    return nodes
+    
+               
 
 def extract_markdown_links(text):
      return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_markdownhandler.py
+++ src/test_markdownhandler.py
@@ -6,9 +6,9 @@ from markdownhandler import *
 class TestMarkdownHandler(unittest.TestCase):
 
     def test_split_delimiter_default(self):
-        node = TextNode("Sample text with **bold text** in it.", TextType.TEXT)
+        node = TextNode("Sample text with **bold** in it.", TextType.TEXT)
         test_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertEqual(test_nodes, [TextNode('Sample text with ', TextType.TEXT, None), TextNode('bold text', TextType.BOLD, None), TextNode(' in it.', TextType.TEXT, None)])
+        self.assertEqual(test_nodes, [TextNode('Sample text with ', TextType.TEXT, None), TextNode('bold', TextType.BOLD, None), TextNode(' in it.', TextType.TEXT, None)])
 
     def test_split_delimiter_not_text(self):
         node = TextNode("Italicized sentence!", TextType.ITALIC)
@@ -25,6 +25,14 @@ class TestMarkdownHandler(unittest.TestCase):
     def test_markdown_url(self):
         text = "I think [Boot.dev](https://www.boot.dev/) is pretty cool."
         self.assertEqual(extract_markdown_links(text), [('Boot.dev', 'https://www.boot.dev/')])
+
+    def test_split_img(self):
+        node = TextNode("This is text with a link ![to a random image](https://www.boot.dev/test.png) and ![to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        split_nodes_image([node])
+
+    def test_split_img_no_imgs(self):
+        node = TextNode("This text has no images in it!", TextType.TEXT)
+        self.assertEqual(split_nodes_image([node]), [TextNode('This text has no images in it!', TextType.TEXT, None)])
 
 if __name__ == "__main__":
     unittest.main()
