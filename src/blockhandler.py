8a932f9dd6df75695b89d3c76169b80f4c60980b
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-02-03 00:17:05 -0600

    regex time

--- src/blockhandler.py
+++ src/blockhandler.py
@@ -1,5 +1,14 @@
 from textnode import *
 import re
+import enum
+
+class BlockType(enum):
+    HEADING = r"^#{1,6}\s.*"
+    CODE = r"^`{3}.*\s*`{3}$"
+    QUOTE = r"^>"
+    ULIST = r"^[\*|-]\s"
+    OLIST = r"^(\d+).\s"
+    
 
 def markdown_to_block(markdown):
     blocks = []
@@ -9,3 +18,17 @@ def markdown_to_block(markdown):
         if res != "":
             blocks.append(res)
     return blocks
+
+def block_to_block_type(block):
+    type = "paragraph"
+    if re.search(BlockType.HEADING, block):
+        type = "heading"
+    elif re.search(BlockType.CODE, block):
+        type = "code"
+    elif re.search(BlockType.QUOTE, block):
+        type = "quote"
+    elif re.search(BlockType.ULIST, block):
+        type = "unordered list"
+    elif re.search(BlockType.OLIST, block):
+        type = "ordered list"
+    return type
