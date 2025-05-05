9d9e711bcf8fa0565610ef6779867aa6b5d76c5f
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-05-05 13:44:34 -0500

    block type check function

--- src/blockhandler.py
+++ src/blockhandler.py
@@ -1,14 +1,14 @@
 from textnode import *
 import re
-import enum
+from enum import Enum
 
-class BlockType(enum):
-    HEADING = r"^#{1,6}\s.*"
-    CODE = r"^`{3}.*\s*`{3}$"
-    QUOTE = r"^>"
-    ULIST = r"^[\*|-]\s"
-    OLIST = r"^(\d+).\s"
-    
+class BlockType(Enum):
+    PARA = "paragraph"
+    HEAD = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UO_LIST = "unordered_list"
+    O_LIST = "ordered_list"
 
 def markdown_to_block(markdown):
     blocks = []
@@ -20,15 +20,25 @@ def markdown_to_block(markdown):
     return blocks
 
 def block_to_block_type(block):
-    type = "paragraph"
-    if re.search(BlockType.HEADING, block):
-        type = "heading"
-    elif re.search(BlockType.CODE, block):
-        type = "code"
-    elif re.search(BlockType.QUOTE, block):
-        type = "quote"
-    elif re.search(BlockType.ULIST, block):
-        type = "unordered list"
-    elif re.search(BlockType.OLIST, block):
-        type = "ordered list"
-    return type
+    heading = r"^#{1,6} .*$"
+    code = r"^```[\s\S]*```$"
+    quote = r"^>.*$"
+    unordered = r"^- .*$"
+    ordered = r"^\d+\. .*$"
+    order_nums = r"^(\d+)\. .*$"
+
+    block_lines = block.split('\n')
+
+    if re.match(heading, block):
+        return BlockType.HEAD
+    if re.match(code, block):
+        return BlockType.CODE
+    if all(re.match(quote, line) for line in block_lines):
+        return BlockType.QUOTE
+    if all(re.match(unordered, line) for line in block_lines):
+        return BlockType.UO_LIST
+    if all(re.match(ordered, line) for line in block_lines):
+        numbers = [int(re.match(order_nums, line).group(1)) for line in block_lines]
+        if numbers[0] == 1 and all(numbers[i] == numbers[i-1] + 1 for i in range(1, len(numbers))):
+            return BlockType.O_LIST
+    return BlockType.PARA
